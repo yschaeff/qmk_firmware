e76ae340987eadcc2ce777c01a54aa587813f226
Sadek Baroudi <sadekbaroudi@gmail.com>
2023-03-23 08:15:05 -0700

    add mouse keys to my personal layout for tenbit

--- keyboards/fingerpunch/tenbit/keymaps/sadekbaroudi/keymap.c
+++ keyboards/fingerpunch/tenbit/keymaps/sadekbaroudi/keymap.c
@@ -27,9 +27,9 @@
   ) \
   LAYOUT_ortho_5x12_wrapper( \
         _______,        KC_1,           KC_2,           KC_3,           KC_4,                   KC_5,                 KC_6,                  KC_7,                  KC_8,              KC_9,           KC_0,            _______, \
-        _______,        K01,            K02,            K03,            LT(_FUNCTION, K04),     K05,                  K06,                   LT(_FUNCTION, K07),    K08,               K09,            K0A,             _______, \
-        _______,        LCTL_T(K11),    LGUI_T(K12),    LALT_T(K13),    LSFT_T(K14),            K15,                  LT(_MOUSE, K16),       RSFT_T(K17),           RALT_T(K18),       RGUI_T(K19),    RCTL_T(K1A),     _______, \
-        _______,        K21,            K22,            K23,            K24,                    K25,                  K26,                   K27,                   K28,               K29,            K2A,             _______, \
+        FP_SCROLL_TOG,  K01,            K02,            K03,            LT(_FUNCTION, K04),     K05,                  K06,                   LT(_FUNCTION, K07),    K08,               K09,            K0A,             _______, \
+        KC_MS_BTN1,     LCTL_T(K11),    LGUI_T(K12),    LALT_T(K13),    LSFT_T(K14),            K15,                  LT(_MOUSE, K16),       RSFT_T(K17),           RALT_T(K18),       RGUI_T(K19),    RCTL_T(K1A),     _______, \
+        KC_MS_BTN2,     K21,            K22,            K23,            K24,                    K25,                  K26,                   K27,                   K28,               K29,            K2A,             _______, \
         _______,        _______,        _______,        K33,            LT(_NAVIGATION,K34),    LT(_FUNCTION,K35),    LT(_MEDIA,K36),        LT(_SYMBOLS,K37),      K38,               _______,        _______,         _______ \
     )
 
