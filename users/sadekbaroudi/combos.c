99634828492b9beb37ab8bfcf7d392fc46023669
Sadek Baroudi <sadekbaroudi@gmail.com>
2023-04-02 22:40:59 -0700

    reduced firmware size a bit for my personal userspace

--- users/sadekbaroudi/combos.c
+++ users/sadekbaroudi/combos.c
@@ -6,20 +6,13 @@
 
 // COMBOS - https://beta.docs.qmk.fm/using-qmk/software-features/feature_combo
 const uint16_t PROGMEM undo_combo[] = {KC_Z, KC_X, COMBO_END};
-//const uint16_t PROGMEM redo_combo[] = {KC_C, KC_D, COMBO_END};
-//const uint16_t PROGMEM enter_combo[] = {KC_K, KC_H, COMBO_END};
-//const uint16_t PROGMEM delete_combo[] = {KC_D, KC_V, COMBO_END};
-//const uint16_t PROGMEM backspace_combo[] = {KC_M, KC_N, COMBO_END};
-//const uint16_t PROGMEM apostrophe_combo[] = {KC_Z, KC_SLSH, COMBO_END};
-//const uint16_t PROGMEM tab_combo[] = {KC_COMMA, KC_SLSH, COMBO_END};
-//const uint16_t PROGMEM capsword_combo[] = {KC_X, KC_C, COMBO_END};
+#ifdef KEYBOARD_fingerpunch_arachnophobe
 const uint16_t PROGMEM q_combo[] = {KC_X, KC_C, COMBO_END};
 const uint16_t PROGMEM p_combo[] = {LSFT_T(KC_T), KC_G, COMBO_END};
 const uint16_t PROGMEM b_combo[] = {KC_D, KC_V, COMBO_END};
 const uint16_t PROGMEM j_combo[] = {KC_K, KC_H, COMBO_END};
 const uint16_t PROGMEM l_combo[] = {LT(_MOUSE, KC_M), RSFT_T(KC_N), COMBO_END};
 const uint16_t PROGMEM semicolon_combo[] = {KC_COMMA, KC_DOT, COMBO_END};
-
 const uint16_t PROGMEM lbracket_combo[] = {LGUI_T(KC_R), KC_X, COMBO_END};
 const uint16_t PROGMEM lcurly_combo[] = {LALT_T(KC_S), KC_C, COMBO_END};
 const uint16_t PROGMEM lparen_combo[] = {LSFT_T(KC_T), KC_D, COMBO_END};
@@ -28,16 +21,11 @@ const uint16_t PROGMEM rangle_combo[] = {LT(_MOUSE, KC_M), KC_K, COMBO_END};
 const uint16_t PROGMEM rparen_combo[] = {RSFT_T(KC_N), KC_H, COMBO_END};
 const uint16_t PROGMEM rcurly_combo[] = {RALT_T(KC_E), KC_COMMA, COMBO_END};
 const uint16_t PROGMEM rbracket_combo[] = {RGUI_T(KC_I), KC_DOT, COMBO_END};
+#endif
 
 combo_t key_combos[COMBO_COUNT] = {
   [UNDO] = COMBO_ACTION(undo_combo),
-//  [REDO] = COMBO_ACTION(redo_combo),
-//  [ENTER] = COMBO_ACTION(enter_combo),
-//  [DELETE] = COMBO_ACTION(delete_combo),
-//  [BACKSPACE] = COMBO_ACTION(backspace_combo),
-//  [APOSTROPHE] = COMBO_ACTION(apostrophe_combo),
-//  [TAB] = COMBO_ACTION(tab_combo),
-//  [CAPSWORD] = COMBO_ACTION(capsword_combo),
+#ifdef KEYBOARD_fingerpunch_arachnophobe
   [Q] = COMBO_ACTION(q_combo),
   [P] = COMBO_ACTION(p_combo),
   [B] = COMBO_ACTION(b_combo),
@@ -52,6 +40,7 @@ combo_t key_combos[COMBO_COUNT] = {
   [RPAREN] = COMBO_ACTION(rparen_combo),
   [RCURLY] = COMBO_ACTION(rcurly_combo),
   [RBRACKET] = COMBO_ACTION(rbracket_combo),
+#endif
 };
 
 void process_combo_event(uint16_t combo_index, bool pressed) {
@@ -63,49 +52,7 @@ void process_combo_event(uint16_t combo_index, bool pressed) {
         tap_code16(LCTL(KC_Z));
       }
       break;
-/*
-    case REDO:
-      if (pressed) {
-        tap_code16(LCTL(KC_Y));
-      }
-      break;
-    case ENTER:
-      if (pressed) {
-        tap_code16(KC_ENT);
-      }
-      break;
-    case DELETE:
-      if (pressed) {
-        tap_code16(KC_DEL);
-      }
-      break;
-    case BACKSPACE:
-      if (pressed) {
-        tap_code16(KC_BSPC);
-      }
-      break;
-    case APOSTROPHE:
-      if (pressed) {
-        if (get_mods() & MOD_MASK_SHIFT) {
-          kc = KC_DOUBLE_QUOTE;
-        } else {
-          kc = KC_QUOTE;
-        }
-        tap_code16(kc);
-      }
-      break;
-    case TAB:
-      if (pressed) {
-        tap_code16(KC_TAB);
-      }
-      break;
-    case CAPSWORD:
-      // NOTE: if you change this behavior, may want to update in process_records.c for capsword macro behavior
-      if (pressed) {
-        enable_caps_word();
-      }
-      break;
-*/
+#ifdef KEYBOARD_fingerpunch_arachnophobe
     case Q:
       if (pressed) {
         tap_code16(KC_Q);
@@ -176,7 +123,8 @@ void process_combo_event(uint16_t combo_index, bool pressed) {
         tap_code16(RBRACKET);
       }
       break;
+#endif
   }
 }
 // END COMBOS
-#endif
+#endif // stenographobe exclusion
