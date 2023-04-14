bd1c91cb5ae2c084a9fb4f5b803cb4f6a40463d7
Sadek Baroudi <sadekbaroudi@gmail.com>
2023-04-13 22:49:14 -0700

    added totem to the personal collection

---
+++ keyboards/fingerpunch/personal/totem/keymaps/sadekbaroudi/config.h
@@ -0,0 +1,13 @@
+#pragma once
+
+/* The way how "handedness" is decided (which half is which),
+see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
+for more options.
+*/
+
+#define MASTER_LEFT
+// #define MASTER_RIGHT
+
+#undef LOCKING_SUPPORT_ENABLE
+#undef LOCKING_RESYNC_ENABLE
+
