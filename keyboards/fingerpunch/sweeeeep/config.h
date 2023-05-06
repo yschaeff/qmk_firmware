7a45d03c282b0e1a53159fe8b9c6d4872de36975
Sadek Baroudi <sadekbaroudi@gmail.com>
2023-05-06 01:35:45 -0700

    changing default to MASTER_LEFT instead of EE_HANDS for the sweeeeeep

--- keyboards/fingerpunch/sweeeeep/config.h
+++ keyboards/fingerpunch/sweeeeep/config.h
@@ -89,7 +89,7 @@
 #define ENCODERS_PAD_B_RIGHT {D0}
 #endif
 
-#define EE_HANDS
+#define MASTER_LEFT
 
 /* Backwards compatibility with existing out-of-tree keymaps */
 #define LAYOUT_sweeeeep LAYOUT_split_3x5_3
