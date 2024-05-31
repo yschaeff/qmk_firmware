4a88c101ec98fd43a0d15b3d47624133a977bdb0
Sadek Baroudi <sadekbaroudi@gmail.com>
2024-05-30 19:19:12 -0700

    fixed pointing device orientation on my personal ffkb lite thumb trackball build

--- keyboards/fingerpunch/ffkb/lite/v1/keymaps/sadekbaroudi/config.h
+++ keyboards/fingerpunch/ffkb/lite/v1/keymaps/sadekbaroudi/config.h
@@ -1,14 +1,20 @@
-// bin/fp_build.sh -k ffkb/lite/v1 -m sadekbaroudi -i -r -e "FP_CONVERT_TO=svlinky VIK_PMW3360=yes"
-#ifdef VIK_TRACKBALL_ENABLE
+// bin/fp_build.sh -k ffkb/lite/v1 -m sadekbaroudi -i -r -e "FP_CONVERT_TO=svlinky_v01 VIK_PMW3360=yes"
+#if defined(VIK_TRACKBALL_ENABLE) && defined(FP_CONVERT_TO_SVLINKY_V01)
 #define POINTING_DEVICE_INVERT_Y
 #endif
 
-// bin/fp_build.sh -k ffkb/lite/v1 -m sadekbaroudi -i -r -e "FP_CONVERT_TO=svlinky VIK_PER56_CIRQUE_LEDS=yes"
-#ifdef VIK_PER56_CIRQUE_LEDS
+// bin/fp_build.sh -k ffkb/lite/v1 -m sadekbaroudi -i -r -e "FP_CONVERT_TO=svlinky_v01 VIK_PER56_CIRQUE_LEDS=yes"
+#if defined(VIK_PER56_CIRQUE_LEDS) && defined(FP_CONVERT_TO_SVLINKY_V01)
 #define CIRQUE_PINNACLE_DIAMETER_MM 35
 #define CIRQUE_PINNACLE_ATTENUATION EXTREG__TRACK_ADCCONFIG__ADC_ATTENUATE_2X
 #define CIRQUE_PINNACLE_TAP_ENABLE
 #define POINTING_DEVICE_ROTATION_90
 #endif
 
-// bin/fp_build.sh -k ffkb/lite/v1 -m sadekbaroudi -i -r -e "FP_CONVERT_TO=svlinky VIK_EC11_EVQWGD001=yes VIK_RGB_ONLY=yes RGBLIGHT_ENABLE=yes"
+// bin/fp_build.sh -k ffkb/lite/v1 -m sadekbaroudi -i -r -e "FP_CONVERT_TO=svlinky_v01 VIK_EC11_EVQWGD001=yes VIK_RGB_ONLY=yes RGBLIGHT_ENABLE=yes"
+
+// 15mm thumb trackball build
+// bin/fp_build.sh -k ffkb/lite/v1 -m sadekbaroudi -i -r -e "FP_CONVERT_TO=svlinky_v02 VIK_PMW3360=yes"
+#if defined(VIK_TRACKBALL_ENABLE) && defined(FP_CONVERT_TO_SVLINKY_V02)
+#define POINTING_DEVICE_INVERT_X
+#endif
