f941149cbd4eac62e9ee34b46df473635a211600
Yuri Schaeffer <yuri@schaeffer.tk>
2026-03-29 01:15:28 +0100

    stuff

--- keyboards/fingerpunch/ffkb/rp/v1/keymaps/yuri/keymap.c
+++ keyboards/fingerpunch/ffkb/rp/v1/keymaps/yuri/keymap.c
@@ -135,8 +135,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT_ffkb(
   KC_TAB,  KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,         KC_Y,    KC_U,         KC_I,         KC_O,         KC_P,             KC_BSPC,
   KC_ESC,  LSFT_T(KC_A), LCTL_T(KC_S), LALT_T(KC_D), LGUI_T(KC_F), KC_G,         KC_H,    LGUI_T(KC_J), RALT_T(KC_K), RCTL_T(KC_L), RSFT_T(KC_SCLN),  KC_QUOT,
-  _______, KC_Z,         KC_X,         KC_C,         KC_V,         KC_B,         KC_N,    KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,          LGUI(KC_ENT),
-           KC_MUTE,                    _______,      BRACKET,      LOWER,        RAISE,   KC_SPC,       KC_BSLS,                    LCTL(KC_BSPC)
+  _______, KC_Z,         KC_X,         KC_C,         KC_V,         KC_B,         KC_N,    KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,          LCTL(KC_RBRC),
+           KC_MUTE,                    _______,      KC_RALT,      LOWER,        RAISE,   KC_SPC,       KC_BSLS,                    LCTL(KC_BSPC)
 ),
 
 /* plain
@@ -198,7 +198,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = LAYOUT_ffkb(
   KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
-  _______, KC_LABK, KC_LCBR, KC_LBRC, KC_LPRN, _______,      _______, KC_RPRN, KC_RBRC, KC_RCBR, KC_RABK, _______,
+  _______, KC_LABK, KC_LCBR, KC_LBRC, KC_LPRN, KC_UNDS,      KC_MINS, KC_RPRN, KC_RBRC, KC_RCBR, KC_RABK, _______,
   _______, _______, _______, _______, _______, _______,      _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, _______,
            _______,          _______, _______, _______,      _______, _______, _______,          _______
 
